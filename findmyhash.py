fb37604737549a73424c97e74e7cf3080f76dcdc
Quentin POIRIER <quentin.poirier@continental-corporation.com>
2018-04-05 23:35:57 +0200

    changed cli usage a bit in order to make it more user-friendly

--- findmyhash.py
+++ findmyhash.py
@@ -28,31 +28,34 @@ def crack(flags):
 		except AttributeError:
 			pass
 
-def algo_list():
+def algo_list(flags):
 	for algo in sorted(services.Service.get_supported_algos(), key=attrgetter("name")):
 		print(algo.name)
 
-def algo(flags):
-	if flags.COMMAND == "list":
-		algo_list()
+def print_help(flags):
+	flags.parser.print_help()
 
 def main(args):
 	# TODO: Arg parsing is ugly, redo that
 	parser = argparse.ArgumentParser(prog=args[0])
+	parser.set_defaults(handler=print_help, parser=parser)
 	subparsers = parser.add_subparsers(help='sub-command help')
 
 	parser_crack = subparsers.add_parser('crack', help='crack help')
 	parser_crack.add_argument('-f', '--file', action='store_true', help='Indicates that ARG is a file storing hashes, one per')
 	parser_crack.add_argument('-a', '--algo', required=True, action='store', help='Algorithm used to generate the hash')
 	parser_crack.add_argument('ARG', help='Either the file or the hash to be cracked')
-	parser_crack.set_defaults(func=crack)
+	parser_crack.set_defaults(handler=crack, parser=parser_crack)
 
 	parser_algo = subparsers.add_parser('algo', help='crack help')
-	parser_algo.add_argument('COMMAND', help='command to execute', choices=["list"])
-	parser_algo.set_defaults(func=algo)
+	parser_algo.set_defaults(handler=print_help, parser=parser_algo)
+
+	algo_subparsers = parser_algo.add_subparsers(help='sub-commanh help')
+	parser_algo_list = algo_subparsers.add_parser('list', help='list all available algorithms')
+	parser_algo_list.set_defaults(handler=algo_list, parser=parser_algo_list)
 
 	args = parser.parse_args(args[1:])
-	args.func(args)
+	args.handler(args)
 
 if __name__ == "__main__":
     main(sys.argv)
